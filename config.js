c454e0bc53e3da0c5a8923b448a05dd71c48f9d7
Venom-png-coder <mwanzamaurice91@gmail.com>
2024-10-24 18:34:31 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "popkid",
+    OWNER_NAME: process.env.OWNER_NAME || "Candy",
     NUMERO_OWNER : process.env.NUMERO_OWNER || " popkid",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
